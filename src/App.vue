b86b19d7a31a58f62c4a966bda092296e3ab7424
David Ferguson <david@ferg.co>
2024-05-03 21:25:54 -0500

    setup buttons for testing mixins

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,73 @@
 import { useCesium } from './composables/useCesium';
 
 const cesium = useCesium('cesium')
+
+function createPoint(name: string, position: number[]) {
+  return {
+    name,
+    position: { cartographicDegrees: position },
+    point: {
+      color: { rgba: [255, 255, 255, 255] },
+      outlineColor: { rgba: [255, 0, 0, 255] },
+      outlineWidth: 2,
+      pixelSize: 8,
+    },
+  }
+}
+
+async function init() {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+  cesium?.process([
+    createPoint('Huntsville, AL', [-86, 34, 0]),
+    createPoint('Miami, FL', [-80, 26, 0]),
+  ])
+}
+
+init()
+
+async function onSelect() {
+  const entities = await cesium?.viewer?.select()
+  console.log(entities)
+}
+
+async function onDrawCircle() {
+  const result = await cesium?.viewer?.drawcircle()
+  console.log(result)
+}
+
+async function onDrawPolygon() {
+  const result = await cesium?.viewer?.drawpolygon()
+  console.log(result)
+}
+
+async function onMeasure() {
+  const result = await cesium?.viewer?.measure()
+  console.log(result)
+}
+
+async function onPickEntity() {
+  const result = await cesium?.viewer?.pickEntity()
+  console.log(result)
+}
+
+async function onPickLocation() {
+  const result = await cesium?.viewer?.pickLocation()
+  console.log(result)
+}
+
 </script>
 
 <template>
-  <div id="cesium">
+  <div id="cesium"></div>
 
+  <div
+    style="position: absolute; bottom: 45px; left: 50%; transform: translateX(-50%); width: 100%; display: flex; justify-content: center; column-gap: 5px;">
+    <button @click="onSelect">Select</button>
+    <button @click="onDrawCircle">Draw Circle</button>
+    <button @click="onDrawPolygon">Draw Polygon</button>
+    <button @click="onMeasure">Measure</button>
+    <button @click="onPickEntity">Pick Entity</button>
+    <button @click="onPickLocation">Pick Location</button>
   </div>
 </template>
 
--- src/composables/useCesium.ts
+++ src/composables/useCesium.ts
@@ -1,30 +1,50 @@
 import { onMounted, nextTick } from 'vue'
-import { Viewer } from 'cesium'
-import { cursor, tooltip } from '@jdfwarrior/cesium-mixins'
+import { Viewer, CzmlDataSource } from 'cesium'
+import { cursor, controls, selection, tooltip, measure, pickEntity, pickLocation, drawcircle, drawpolygon } from '@jdfwarrior/cesium-mixins'
 
 const instances = new Map<string, CesiumViewer>()
 
 class CesiumViewer {
     viewer: Viewer | undefined
+    czml = new CzmlDataSource('czml')
     constructor(id: string, options: Viewer.ConstructorOptions) {
         onMounted(async () => {
             await nextTick()
             this.viewer = new Viewer(id, options)
-            this.viewer.extend(cursor, { ui: true })
+            this.viewer.extend(cursor)
+            this.viewer.extend(controls)
+            this.viewer.extend(selection)
             this.viewer.extend(tooltip)
+            this.viewer.extend(measure)
+            this.viewer.extend(pickEntity)
+            this.viewer.extend(pickLocation)
+            this.viewer.extend(drawcircle)
+            this.viewer.extend(drawpolygon)
+
+            this.viewer.dataSources.add(this.czml)
+            this.czml.process({ id: "document", version: "1.0" })
         })
+    }
+
+    // @todo replace this data type with the czml packet type
+    process(packets: Record<string, unknown>[]) {
+        this.czml.process(packets)
+        this.viewer?.render()
     }
 }
 
 export const useCesium = (id: string = 'cesium') => {
     const options: Viewer.ConstructorOptions = {
         navigationHelpButton: false,
-        geocoder: false
+        geocoder: false,
+        infoBox: false
+        // requestRenderMode: true
     }
 
     if (!instances.has(id)) {
         const viewer = new CesiumViewer(id, options)
         instances.set(id, viewer)
+        return viewer
     } else {
         return instances.get(id)
     }
